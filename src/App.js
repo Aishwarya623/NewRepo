a8667416a15489c163d96e17c9b85f7db1e2c4c1
Aishwarya623 <paishwarya623.com>
2026-03-25 11:00:15 +0530

    fixed router issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation,useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import { HelmetProvider } from "react-helmet-async";
 import LandingPage from "./views/LandingPage";
 import Services from "./views/Services";
@@ -7,6 +8,16 @@ import FAQ from "./views/FAQ";
 
 const App = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const redirect = params.get("redirect");
+
+    if (redirect) {
+      navigate(redirect);
+    }
+  }, []);
   return (
     <HelmetProvider>
       <Routes location={location} key={location.pathname}>
