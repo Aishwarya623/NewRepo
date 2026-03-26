d49a0656c458097016fe5d4ad559ae3de2864117
Aishwarya623 <paishwarya623.com>
2026-03-26 11:59:18 +0530

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Routes, Route, useLocation } from "react-router-dom";
-// import { useEffect } from "react";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import { HelmetProvider } from "react-helmet-async";
 import LandingPage from "./views/LandingPage";
 import Services from "./views/Services";
